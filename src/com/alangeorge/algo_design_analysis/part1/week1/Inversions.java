d6810e2515bd446b9ed5bbb47368acf24c59fa25
alanwgeorge <ageorge@alan-george.com>
2016-08-22 07:56:21 -0700

    sync

--- src/com/alangeorge/algo_design_analysis/part1/week1/Inversions.java
+++ src/com/alangeorge/algo_design_analysis/part1/week1/Inversions.java
@@ -37,27 +37,45 @@ public class Inversions {
                 return new HashSet<>();
             }
         } else {
-            int[] part1 = new int[ints.length/2];
-            int[] part2 = new int[ints.length/2 + ints.length % 2];
+            int part1Size = ints.length/2 + ints.length % 2;
+            int part2Size = ints.length/2;
+
+            int[] part1 = new int[part1Size];
+            int[] part2 = new int[part2Size];
 
             System.arraycopy(ints, 0, part1, 0, part1.length);
             System.arraycopy(ints, part1.length, part2, 0, part2.length);
 
-            return merge(inversionsR(part1), inversionsR(part2));
+            Set<Inversion> result = new HashSet<>();
+
+            if (part1[part1.length - 1] > part2[0]) {
+                result.add(new Inversion(part1[part1.length - 1], part2[0]));
+            }
+            result.addAll(inversionsR(part1));
+            result.addAll(inversionsR(part2));
+            result.addAll(splitInversions(ints, part1Size));
+
+            return result;
         }
     }
 
-    public static Set<Inversion> merge(Set<Inversion> left, Set<Inversion> right) {
-        count++;
-        System.out.println("Inversions.merge");
-        Set<Inversion> result = new HashSet<>(left);
-        result.addAll(right);
-        return result;
+    public static Set<Inversion> splitInversions(int[] ints, int split) {
+        Set<Inversion> results = new HashSet<>();
+
+        int part1i = 0;
+        int part2i = split;
+
+        for (int i = 0; i < ints.length; i++) {
+
+        }
+
+        return results;
     }
 
     public static void main(String[] args) {
 //        int[] in = {1, 3, 5, 2, 4, 6};
-        int[] in = {1, 5, 3, 2, 6, 4};
+//        int[] in = {1, 5, 3, 2, 6, 4};
+        int[] in = {8, 7, 6, 5, 4, 3, 2, 1};
 
         Set<Inversion> inversions = Inversions.inversionsR(in);
 
